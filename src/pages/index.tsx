e9d953779b63a7e1d62a31893dfc1d9f092bfb63
Jinyeong Kim <kid4211@gmail.com>
2025-07-07 12:19:41 +0900

    SSG - 정적 경로에 적용하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
+import { useEffect } from "react";
+import { InferGetStaticPropsType } from "next";
 import SearchableLayout from "@/components/SearchableLayout";
 import BookItem from "@/components/BookItem";
 import style from "@/pages/index.module.css";
-import { useEffect } from "react";
-import { InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetchBooks";
 import fetchRandomBooks from "@/lib/fetchRandomBooks";
 
@@ -11,7 +11,9 @@ import fetchRandomBooks from "@/lib/fetchRandomBooks";
  * 2. 오직 Server Side에서만 실행되는 함수로, 단 한번 호출되며 브라우져를 새로고침하여도 재실행 되지 않는다.
  * 3. 해당 페이지의 컴포넌트보다 먼저 실행되어서, 필요한 데이터를 API 통신으로 부터 가져올 수 있다.
  */
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("SSG - 인덱스 페이지");
+
   const [allBooks, recommendBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -29,7 +31,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recommendBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   /**
    * Next.js는 페이지를 서버에서 먼저 렌더링한 후 클라이언트로 전송
    * Node.js 서버 환경에서는 window가 정의되지 않기 때문에 undefined 에러가 발생한다.
